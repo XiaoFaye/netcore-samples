3a8be88838300836398d643830324120bfee6b05
XiaoFaye <xiaofaye@msn.com>
2016-05-17 09:58:07 +1200

    7.SQLServer

---
+++ code-samples/7.SQLServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json");
+
+            IConfigurationRoot Configuration = builder.Build();
+			
+			string message = string.Empty;
+			using (SqlConnection conn = new SqlConnection(Configuration["Data:DefaultConnection:ConnectionString"]))
+            {
+                try
+                {
+                    using (SqlCommand cmd = new SqlCommand("SELECT @@version;", conn))
+                    {
+                        conn.Open();
+                        var result = cmd.ExecuteScalar();
+                        if (result != null)
+                            message = result.ToString();
+
+                        conn.Close();
+                    }
+                }
+                catch(Exception ex)
+                {
+                    message = ex.Message;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+			
+			Console.WriteLine("Connect to SQL Server: \n\r" + message);
+        }
+    }
+}
---
+++ code-samples/7.SQLServer/Readme.md
@@ -0,0 +1,9 @@
+﻿7. SQLServer
+======================
+
+## How to run
+
+1. Navigate to the project folder
+2. Change the connection string in appsettings.json file.
+3. run `dotnet restore` in the folder
+4. run `dotnet run`
