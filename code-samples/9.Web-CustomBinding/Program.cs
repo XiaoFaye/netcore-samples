ab4bbd09b63b91e374df2432e1cf37bf6ab65416
XiaoFaye <xiaofaye@msn.com>
2016-05-18 09:47:04 +1200

    9.Web-CustomBinding

---
+++ code-samples/9.Web-CustomBinding/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+	    var host = new WebHostBuilder()
+                        .UseKestrel()
+						.UseUrls("http://www.binding1.co.nz:5100;http://www.binding2.co.nz:5200")
+                        .UseContentRoot(Directory.GetCurrentDirectory())
+                        .UseIISIntegration()
+                        .UseStartup<Startup>()
+                        .Build();
+
+            host.Run();
+        }
+    }
+}
