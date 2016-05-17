9bb7d93986a9424c106c2b1e48e30a116f6c7c47
XiaoFaye <xiaofaye@msn.com>
2016-05-17 21:04:13 +1200

    8.EnvironmentVariables

---
+++ code-samples/8.EnvironmentVariables/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+			List<string> vars = new List<string>();
+			
+			foreach (var key in Environment.GetEnvironmentVariables().Keys)
+			{
+				vars.Add(key + ": ");
+			}
+			
+			int i = 0;
+			foreach (var value in Environment.GetEnvironmentVariables().Values)
+			{
+				vars[i] += value;
+				i++;
+			}
+			
+			foreach(string v in vars)
+				Console.WriteLine(v);
+        }
+    }
+}
