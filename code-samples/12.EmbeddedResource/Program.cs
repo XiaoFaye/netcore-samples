da046051348a239a39f2f411471ca650f492a365
XiaoFaye <xiaofaye@msn.com>
2016-05-24 18:12:52 +1200

    12.EmbeddedResource

---
+++ code-samples/12.EmbeddedResource/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Reflection;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var assembly = Assembly.GetEntryAssembly();
+			
+			Console.WriteLine("All embedded resources:");
+			foreach(string name in assembly.GetManifestResourceNames())
+			{
+				Console.WriteLine(name);
+				var resourceStream = assembly.GetManifestResourceStream(name);
+				if(Path.GetExtension(name).ToLower() == ".txt")
+				{
+					using (var reader = new StreamReader(resourceStream))
+					{
+						Console.WriteLine("Resource content:" + reader.ReadToEnd());
+						Console.WriteLine("Resource size:" + reader.BaseStream.Length.ToString() + " bytes.");
+					}
+				}
+				else
+				{
+					using (var reader = new BinaryReader(resourceStream))
+					{
+						Console.WriteLine("Resource size:" + reader.BaseStream.Length.ToString() + " bytes.");
+					}
+				}	
+				
+				
+				
+			}
+        }
+    }
+}
