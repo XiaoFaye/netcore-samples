d5d3290ae81045e6981102aecae2eafd54ad3e5a
James Yang <xiaofaye@msn.com>
2017-08-30 09:01:31 +1200

    Targeting .NET Core 2.0

--- code-samples/1.HelloWorld/Program.cs
+++ code-samples/1.HelloWorld/Program.cs
@@ -2,9 +2,9 @@
 
 namespace ConsoleApplication
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
--- code-samples/1.HelloWorld/Readme.md
+++ code-samples/1.HelloWorld/Readme.md
@@ -4,5 +4,4 @@
 ## How to run
 
 1. Navigate to the project folder
-2. run `dotnet restore` in the folder
-3. run `dotnet run`
+2. run `dotnet run`
